5329c5a4d80268751e8921a53dec480f1654d72c
jh31barbosa <vazavult@gmail.com>
2022-09-22 20:31:29 -0300

    d

--- room/routing.py
+++ room/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('wss/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
 ]
