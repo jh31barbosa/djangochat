b974d1d0bad4a60ad80d8971564ef8d49d58b73b
jh31barbosa <vazavult@gmail.com>
2022-09-21 22:02:42 -0300

    change on the settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+import django_on_heroku
 
 from pathlib import Path
 from telnetlib import LOGOUT
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
